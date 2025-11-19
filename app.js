9931e935d197e51f52cbba6bab4749c55d0d69f9
Pau <paulina.floress@iteso.mx>
2025-11-19 13:33:14 -0600

    Archivo yml modificado

---
+++ app.js
@@ -0,0 +1,13 @@
+const moment = require("moment");
+
+let i = 5;
+
+let j = 10;
+
+console.log('Iniciando...');
+
+console.log("La suma es " + (5+10));
+ 
+let fecha = moment().format("LL");
+
+console.log("Y la fecha es:", fecha);
